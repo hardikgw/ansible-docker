cd298527aefafcdacd31e672606eb94c757a78a5
Hardik Patel <hardikgw@gmail.com>
2016-08-11 06:58:52 -0400

    change password script updates

--- simple/conf/mysql-change-pswd.sql
+++ simple/conf/mysql-change-pswd.sql
@@ -1,1 +1,3 @@
-SET PASSWORD FOR 'demo'@'%'='@newdbpswd';
+SET @changepswd = CONCAT('SET PASSWORD FOR "','demo','"@"%" = "',@newdbpswd,'" ');
+PREPARE stmt FROM @changepswd; EXECUTE stmt; DEALLOCATE PREPARE stmt;
+SELECT @newdbpswd, @changepswd;
