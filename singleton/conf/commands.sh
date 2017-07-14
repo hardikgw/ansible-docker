9a0e4b02dd626b5e7a1bc73a3b0d4d33b57e1973
Hardik Patel <hardikgw@gmail.com>
2017-07-14 15:03:58 -0400

    uri tasks

--- singleton/conf/commands.sh
+++ singleton/conf/commands.sh
@@ -1,6 +1,10 @@
+#!/bin/sh
+
 docker exec mq apt-get update
 docker exec mq apt-get install -y openssh-server
 docker exec mq service ssh start
+docker exec web1 /usr/sbin/sshd -D
+docker exec web2 /usr/sbin/sshd -D
 
 
 docker cp ansible_hosts ansible:/etc/ansible/hosts
