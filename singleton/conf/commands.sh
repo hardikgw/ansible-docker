d981a83fe8b0b6fb1b1f7fbcf22d17f9b94502ac
Hardik Patel <hardikgw@gmail.com>
2017-07-16 21:51:31 -0400

    ansible loop

--- singleton/conf/commands.sh
+++ singleton/conf/commands.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-docker exec mq apt-get update
-docker exec mq apt-get install -y openssh-server
-docker exec mq service ssh start
-docker exec web1 /usr/sbin/sshd -D
-docker exec web2 /usr/sbin/sshd -D
+#docker exec mq service ssh start
+docker exec web1 service ssh start
+docker exec web2 service ssh start
 
 
-docker cp ansible_hosts ansible:/etc/ansible/hosts
-docker cp ping.yml ansible:/ping.yml
+#docker cp ansible_hosts ansible:/etc/ansible/hosts
+#docker cp ping.yml ansible:/ping.yml
 
-docker exec ansible ansible-playbook ping.yml
+docker cp . ansible:/.
+docker exec ansible ansible-playbook listener.yml
