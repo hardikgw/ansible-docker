3c1fec8dfd552e92dd9134fb02be848f8714de91
Hardik Patel <hardikgw@gmail.com>
2017-07-21 09:30:42 -0400

    script

--- singleton/conf/monitor.sh
+++ singleton/conf/monitor.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-export WEB1="http://localhost:8081"
-export WEB2="http://localhost:8082"
+#export WEB1="http://localhost:8081"
+#export WEB2="http://localhost:8082"
+
+if [ -z "$WEB1" ] || [ -z "$WEB2" ]; then
+    echo "WEB1, WEB2 not defined"
+    exit 1
+fi
 
 function runCommand() {
 	curl POST --url $1/jms/admin -H "Content-Type: text/plain" -d $2 -s -f
@@ -15,17 +20,15 @@ function checkStatus() {
 }
 checkStatus
 
-if [ $WEB1Status != "-ok" ]
-then
+if [ $WEB1Status != "-ok" ]; then
 	echo WEB1 $(runCommand $WEB1 start)
 fi
 
 checkStatus
 
-if [ $WEB1Status != "-ok" ]
-then
+if [ $WEB1Status != "-ok" ] && [ $WEB2Status != "-ok" ]; then
 	echo WEB2 $(runCommand $WEB2 start)
-else
+elif [ $WEB1Status = "-ok" ] && [ $WEB2Status = "-ok" ]; then
 	echo WEB2 $(runCommand $WEB2 stop)
 fi
 
